de68205e2506f76aa4b53582e2c8751937a36067
yunus <karattyunus@gmail.com>
2025-03-11 00:21:40 +0530

    config changes

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -88,7 +88,7 @@ const search = () => {
         ListEmptyComponent={
           !moviesLoading && !moviesError ? (
             <View className="mt-10 px-5">
-              <Text className="text-center text-gray-500">
+              <Text className="text-center text-gray-500 ">
                 {searchQuery.trim() ? "No movies found" : "Search for a movie"}
               </Text>
             </View>
